6c897faf1f663cf47b0fe22e2e55a66fdc5df7c1
Diego <149540779+DiegoOrtegaGarcia@users.noreply.github.com>
2026-01-02 14:19:11 -0500

    add the Basic Concept for the  Home Page

--- __tests__/modules/Home/containers/HomeContainer.test.tsx
+++ __tests__/modules/Home/containers/HomeContainer.test.tsx
@@ -1,11 +1,20 @@
 import HomeContainer from '@/modules/Home/container/HomeContainer';
 import '@testing-library/jest-dom'
-import {render, screen} from '@testing-library/react'
+import {render} from '@testing-library/react'
  
 describe('Home', () => {
   it('renders a heading', () => {
      const component = render(<HomeContainer/>);
  
     component.getAllByText("Chest Game")
+  })
+
+  it("render a button",()=>{
+    const component = render(<HomeContainer/>);
+
+    const link = component.getByText('Play a Game');
+
+    expect(link.tagName).toBe('A');
+    expect(link).toHaveAttribute('href', '/ChestGame');
   })
 })
--- modules/Home/container/HomeContainer.tsx
+++ modules/Home/container/HomeContainer.tsx
@@ -1,9 +1,14 @@
+import Link from 'next/link'
+
 export default function HomeContainer(){
     return (
-        <div>
-            <main>
-                Hi
-            </main>
-        </div>
+        <main className="flex flex-col  items-center p-8  w-screen h-screen justify-around">
+            <div className="p-5 shadow-2xl shadow-red-500 rounded-4xl border-red-800 border">
+                <h1 className="text-red-800 text-2xl md:text-8xl text-center font-bold ">Chest Game</h1>
+            </div>
+            <div>
+                <Link href="/ChestGame" className="bg-red-800 p-3 text-2xl rounded-3xl hover:text-red-600 hover:bg-red-950">Play a Game</Link>
+            </div>
+        </main>
     )
 }
